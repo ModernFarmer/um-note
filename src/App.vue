c291bd9687e780e56037741a9359f17f0715e8c3
ModernFarmer <47311260+ModernFarmer@users.noreply.github.com>
2021-07-05 18:40:40 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
